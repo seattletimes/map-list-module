fbd72d368748ddc55e9cc833c6b0d04c7a625e0e
Ava Mandoli <avamandoli@gmail.com>
2025-09-26 12:30:45 -0700

    basic leaflet map and market loaded

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 // var paywall = require("./lib/paywall");
 // setTimeout(() => paywall(12345678), 5000);
 
+import L from 'leaflet';
 //added sidechain 
 import Sidechain from '@nprapps/sidechain';
 const guest = Sidechain.registerGuest({ sentinel: 'st' });
@@ -11,4 +12,18 @@ guest.sendMessage({
   eventAction: 'click',
   eventLabel: 'etc'
 })
+
+console.log(L);
+
+const map = L.map('map')
+map.setView([47.6108, -122.3307], 11);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd'
+}).addTo(map);
+
+L.marker([47.661269, -122.342919], {
+  title: "Meowtropolitan Cafe",
+}).bindPopup('<h2>Meowtropolitan Cafe</h2>').addTo(map)
 //end sidechain
