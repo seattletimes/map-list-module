40ad947b60c373a8f67b1963b45853d441faab30
Ava Mandoli <avamandoli@gmail.com>
2025-10-02 12:43:54 -0700

    addMarkers function added

--- src/js/main.js
+++ src/js/main.js
@@ -16,13 +16,11 @@ guest.sendMessage({
 fetch('../../data/map-module.sheet.json')
   .then(response => response.json())
   .then(data => {
-    const locations = data; // or just use 'data' directly
-    
-    console.log(locations); // Check what you got
-    
-    // Now you can loop through and use it
-    // addMarkers(locations);
+    const locations = data; 
+    addMarkers(locations);
+    addSidebarItem(locations);
   });
+
 
 const map = L.map('map')
 map.setView([47.6108, -122.3307], 11);
@@ -30,11 +28,70 @@ map.setView([47.6108, -122.3307], 11);
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',{     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',     crossOrigin: 'anonymous' }).addTo(map)
 
 
-function addMarkers(){
+let markersArray = []
+
+function addMarkers(locations){
+  for (let i = 0; i < locations.length; i++) {
+    let marker = L.circleMarker([locations[i].lat, locations[i].long], {
+    radius: 8,  // Size of the circle
+    fillColor: "green",
+    color: "darkgreen",  // Border color
+    weight: 2,  // Border width
+    opacity: 1,
+    fillOpacity: 0.8,
+    title: locations[i].name,
+    }).bindPopup(`<h2>${locations[i].name}</h2><n><p>${locations[i].address}</p>`).addTo(map);
+
+    markersArray.push(marker);
+    console.log("markersArray", markersArray)
+  }
 
 }
 
-L.marker([47.661269, -122.342919], {
-  title: "Meowtropolitan Cafe",
-}).bindPopup('<h2>Meowtropolitan Cafe</h2>').addTo(map)
-//end sidechain
+// Populate sidebar
+
+function addSidebarItem(locations){
+  sidebar = document.getElementById("sidebar-ul")
+
+  for (let i = 0; i < locations.length; i++) {
+    var listItem = document.createElement("li");
+    listItem.dataset.index = i;
+    listItem.classList.add("sidebar-li")
+    sidebar.appendChild(listItem)
+
+    // Location title
+    var listItemTitle = document.createElement("h2")
+    listItem.appendChild(listItemTitle)
+
+    listItemTitle.innerText = locations[i].name
+
+    // Location details
+    var listItemDetails = document.createElement("p")
+    listItem.appendChild(listItemDetails)
+
+    listItemDetails.innerText = locations[i].address
+
+  }
+}
+
+function onListItemClick(e) {
+  console.log("listitemclick running")
+  const listItem = e.target.closest('LI')
+
+if (listItem){
+  
+  document.querySelectorAll('.sidebar-li').forEach(item => {
+    item.classList.remove('active');
+  });
+
+  listItem.classList.add('active');
+  console.log("active item:", listItem.innerText);
+  
+} 
+}
+
+window.addEventListener("click", onListItemClick)
+// L.marker([47.661269, -122.342919], {
+//   title: "Meowtropolitan Cafe",
+// }).bindPopup('<h2>Meowtropolitan Cafe</h2>').addTo(map)
+// //end sidechain
